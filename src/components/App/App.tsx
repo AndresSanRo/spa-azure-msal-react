c510bea3002a6d1bd26f5f7f69b497e7e85a87da
Andrés <andressanchezro@hotmail.com>
2021-07-19 22:12:59 +0200

    feat: get access token

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,18 @@
+import React, { useState } from 'react';
 import { NavBar } from '../NavBar';
 import { UserArea } from '../UserArea';
 import './App.css';
 
+export const App: React.FC = () => {
+	const [token, setToken] = useState<string>();
 
-export const App = () => {
-  return (
-    <div className="App">
-      <NavBar><h2>@azure/msal-react</h2><UserArea /></NavBar>   
-      <div className="body">e</div>   
-    </div>
-  );
-}
+	return (
+		<div className="App">
+			<NavBar>
+				<h2>@azure/msal-react</h2>
+				<UserArea setToken={setToken} />
+			</NavBar>
+			<div className="body">ª</div>
+		</div>
+	);
+};
--- src/components/UserArea/UserArea.tsx
+++ src/components/UserArea/UserArea.tsx
@@ -1,16 +1,58 @@
-import { AccountInfo } from "@azure/msal-browser";
-import { useIsAuthenticated } from "@azure/msal-react";
-import { msalInstance } from "../../services/msal/msalConfig";
-import { SignInOutButton } from "../SignInOutButton"
-import './UserArea.css'
+import { AccountInfo, InteractionRequiredAuthError } from '@azure/msal-browser';
+import { useIsAuthenticated } from '@azure/msal-react';
+import { useEffect } from 'react';
+import { loginRequest, msalInstance } from '../../services/msal/msalConfig';
+import { SignInOutButton } from '../SignInOutButton';
+import './UserArea.css';
 
-const getUserName = (): string => {
-    const myAccounts: AccountInfo[] = msalInstance.getAllAccounts();
-    return 'Welcome ' + myAccounts[0].name!
+interface UserAreaProps {
+	setToken: (token: string) => void;
 }
 
-export const UserArea: React.FC = () => {
-    const isAuthenticated = useIsAuthenticated();
+export const UserArea: React.FC<UserAreaProps> = (props) => {
+	const isAuthenticated = useIsAuthenticated();
 
-    return <div className="userArea"><div>{isAuthenticated ? <span>{getUserName()}</span> : <></>}<SignInOutButton /></div></div>
-}
+	useEffect(() => {
+		if (isAuthenticated) {
+			msalInstance
+				.acquireTokenSilent({
+					account: msalInstance.getAllAccounts()[0],
+					scopes: loginRequest.scopes,
+				})
+				.then((token) => {
+					props.setToken(token.accessToken);
+				})
+				.catch((error) => {
+					if (error instanceof InteractionRequiredAuthError) {
+						msalInstance
+							.acquireTokenPopup(loginRequest)
+							.then((token) => {
+								props.setToken(token.accessToken);
+							})
+							.catch((error) => {
+								console.log(error);
+							});
+					} else {
+						console.log(error);
+					}
+				});
+		}
+		return () => {
+			('');
+		};
+	}, [isAuthenticated]);
+
+	const getUserName = (): string => {
+		const myAccounts: AccountInfo[] = msalInstance.getAllAccounts();
+		return 'Welcome ' + myAccounts[0].name!;
+	};
+
+	return (
+		<div className="userArea">
+			<div>
+				{isAuthenticated ? <span>{getUserName()}</span> : <></>}
+				<SignInOutButton />
+			</div>
+		</div>
+	);
+};
